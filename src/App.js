89f423046cd2d68f95f2f07488cf46db75af2b5a
Blue Jessen <canealisblue@gmail.com>
2022-08-04 10:54:55 -0700

    Add tea cards

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
 import logo from './logo.svg';
+import React, { Component } from 'react'
 import './App.css';
+import TeaCard from './components/TeaCard/TeaCard.js'
 
-function App() {
+class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+      teaCards: []
+    }
+  }
+
+   componentDidMount() {
+
+    fetch('https://tea-list-api.herokuapp.com/api/v1/teas')
+    .then(resource => resource.json())
+    .then(data => {
+      let cards = data.teas.map((tea) => {
+          return <TeaCard name={tea.name} how={tea.how} type={tea.type} notes={tea.notes} />
+      })
+      this.setState({teaCards: cards})
+    })
+  }
+
+
+render(){
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='card-container'>
+      {!this.state.teaCards && <h1>..Loading</h1>}
+      {this.state.teaCards}
     </div>
-  );
+  )
+
+}
 }
 
 export default App;
