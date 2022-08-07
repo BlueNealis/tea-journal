f30907956e1665b36c0ad189de4e1147e4b8b14d
Blue Jessen <canealisblue@gmail.com>
2022-08-07 14:57:25 -0700

    Add App skeleton tests

--- cypress/e2e/App.cy.js
+++ cypress/e2e/App.cy.js
@@ -1,5 +1,28 @@
 describe('empty spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
+  beforeEach(() => {
+    cy.visit('localhost:3000')
+  })
+  it('Should welcome user to Tea Journal', () => {
+
+  })
+
+  it('Should show all Teas available', () => {
+
+  })
+
+  it('Should be able to click on a tea to favorite it', () => {
+
+  })
+
+  it('Should be able to click on favorites ', () => {
+
+  })
+
+  it('Should tell user if the server is down', () => {
+
+  })
+
+  it('Should bring user back to homepage if using a url with no Route', ()=> {
+    
   })
 })
--- cypress/e2e/EntryForm.cy.js
+++ cypress/e2e/EntryForm.cy.js
@@ -1,5 +1,5 @@
 describe('empty spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
+  beforeEach('passes', () => {
+    cy.visit('localhost:3000/newEntry')
   })
 })
