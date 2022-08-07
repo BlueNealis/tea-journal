1cd3c2eeb38a9e1ee279e834764ab99cf1ef7f07
Blue Jessen <canealisblue@gmail.com>
2022-08-07 15:13:14 -0700

    Write favorites cypress skeletons

--- cypress/e2e/Favorites.cy.js
+++ cypress/e2e/Favorites.cy.js
@@ -1,5 +1,21 @@
 describe('empty spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
+  beforeEach(() => {
+    cy.visit('localhost:3000/favorites')
+  })
+
+  it('should show all user selected favorites', () => {
+
+  })
+
+  it('should be able to unfavorite teas', () => {
+
+  })
+
+  it('should be able to click on new entry button and be redirected to form page', () => {
+
+  })
+
+  it('should be able to go back to homepage and favorite more and have that be reflected on favorite page', () => {
+
   })
 })
