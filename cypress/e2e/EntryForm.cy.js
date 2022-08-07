257e47006729754d74779f7fd2cab83aef2a4c4b
Blue Jessen <canealisblue@gmail.com>
2022-08-07 15:09:20 -0700

    Add form cypress test skeletons

--- cypress/e2e/EntryForm.cy.js
+++ cypress/e2e/EntryForm.cy.js
@@ -1,5 +1,29 @@
 describe('empty spec', () => {
   beforeEach('passes', () => {
-    cy.visit('localhost:3000/newEntry')
+    cy.visit('localhost:3000/new-entry')
   })
+  it('should be able to select a Tea and update the form to reflect that', () => {
+
+  })
+
+  it('should have all teas in option list on page load', () => {
+
+  })
+
+  it('should be able to fill out form', () => {
+
+  })
+
+  it('should not be able to submit form until all fields have been filled out', () => {
+
+  })
+
+  it('should redirect to teasperiences page when the user submits their entry', () => {
+
+  })
+
+  it('should clear the form after the entry is submitted', () => {
+
+  })
+  
 })
