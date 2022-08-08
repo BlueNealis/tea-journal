f024cb5fa5e80438a3cff78f4ad24fb5ba127ce9
Blue Jessen <canealisblue@gmail.com>
2022-08-07 18:56:46 -0700

    Add teasperiences testing

--- cypress/e2e/Teasperiences.cy.js
+++ cypress/e2e/Teasperiences.cy.js
@@ -1,12 +1,31 @@
 describe('empty spec', () => {
   beforeEach(() => {
-    cy.visit('localhost:3000/teasperiences')
+    cy.visit('localhost:3000')
+    cy.intercept("GET",'https://tea-list-api.herokuapp.com/api/v1/teas',{ fixture: '../fixtures/teas.json'})
+    cy.get('.tea-card').eq(2).find('.entry-button').click()
+    cy.get('form').within(() => {
+      cy.get('select').select('White Peony')
+      cy.get('.date-input').type('2022-08-08')
+      cy.get('.input-experience').type('Eloquent')
+      cy.get('.input-notes').type('fresh, green')
+      cy.get('.submit-button').click()
+    })
+  })
+    cy.contains('HOME').click()
+    cy.get('.tea-card').eq(2).find('.entry-button').click()
+    cy.get('form').within(() => {
+      cy.get('select').select('Irish Breakfast')
+      cy.get('.date-input').type('2022-08-10')
+      cy.get('.input-experience').type('A strong morning start')
+      cy.get('.input-notes').type('earthy, bold')
+      cy.get('.submit-button').click()
+    })
   })
   it('should show all teasperiences', () => {
-
-  })
-
-  it('should be able to add entry and have it reflect in teasperiences', () => {
+    cy.get('.entry-card').should('have.length', 2)
+    cy.should('contain','A strong morning start')
+    .and('contain', 'Eloquent')
+    .and('contain', 'White Peony')
 
   })
 })
