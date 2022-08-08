c6a5f10101a65182c847073b99918715972d8739
Blue Jessen <canealisblue@gmail.com>
2022-08-07 18:38:04 -0700

    Add entry form tests

--- cypress/e2e/EntryForm.cy.js
+++ cypress/e2e/EntryForm.cy.js
@@ -13,23 +13,60 @@ it('should be able to select a Tea and update the form to reflect that', () => {
   })
 
   it('should have all teas in option list on page load', () => {
-
+    cy.get('form').within(() => {
+      cy.get('select').select('Yunnan Gold')
+      cy.get('select').select('Irish Breakfast')
+      cy.get('select').select('Silver Needle')
+    })
   })
 
   it('should be able to fill out form', () => {
-
+    cy.get('form').within(() => {
+      cy.get('select').select('White Peony')
+      cy.get('.date-input').type('2022-08-08')
+      cy.get('.input-experience').type('Eloquent')
+      cy.get('.input-notes').type('fresh, green')
+      cy.get('.submit-button').click()
+    })
   })
 
   it('should not be able to submit form until all fields have been filled out', () => {
-
+    cy.get('form').within(() => {
+      cy.get('select').select('White Peony')
+      cy.get('.date-input').type('2022-08-08')
+      cy.get('.input-notes').type('fresh, green')
+      cy.get('.submit-button').click()
+    })
+    cy.contains('Please fill out all fields')
+    cy.url('localhost:3000/new-entry')
   })
 
   it('should redirect to teasperiences page when the user submits their entry', () => {
-
+    cy.get('form').within(() => {
+      cy.get('select').select('White Peony')
+      cy.get('.date-input').type('2022-08-08')
+      cy.get('.input-experience').type('Eloquent')
+      cy.get('.input-notes').type('fresh, green')
+      cy.get('.submit-button').click()
+    })
+      cy.url('localhost:3000/teasperiences')
   })
 
   it('should clear the form after the entry is submitted', () => {
-
+    cy.get('form').within(() => {
+      cy.get('select').select('White Peony')
+      cy.get('.date-input').type('2022-08-08')
+      cy.get('.input-experience').type('Eloquent')
+      cy.get('.input-notes').type('fresh, green')
+      cy.get('.submit-button').click()
+    })
+      cy.url('localhost:3000/teasperiences')
+      cy.visit('localhost:3000/new-entry')
+      cy.get('form').within(() => {
+        cy.get('.date-input').should('be.empty')
+        cy.get('.input-experience').should('be.empty')
+        cy.get('.input-notes').should('be.empty')
+      })
   })
 
 })
