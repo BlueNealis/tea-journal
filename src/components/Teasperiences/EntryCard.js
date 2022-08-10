b9b567868d1731dde89b899921fac74d749282be
Blue Jessen <canealisblue@gmail.com>
2022-08-09 19:31:06 -0700

    Add basic styling to entry card

--- src/components/Teasperiences/EntryCard.js
+++ src/components/Teasperiences/EntryCard.js
@@ -5,12 +5,16 @@ import './EntryCard.css'
 export default function EntryCard ({teaType, date, description, notes}) {
     return(
       <div className='entry-card'>
-        <h1>{teaType}</h1>
-        <h2>{date}</h2>
+        <div className='entry-header'>
+          <h1 style={{marginRight: '4vw'}}>{teaType}</h1>
+          <h2 style={{marginLeft: '6vw'}}>{date}</h2>
+          <button style={{marginLeft:'10vw', border:'none'}}>Edit</button>
+          <button style={{marginLeft:'1vw', border:'none'}}>X</button>
+        </div>
         <article>
           <p>{description}</p>
+          <p>{notes}</p>
         </article>
-        <p>{notes}</p>
       </div>)
 }
 
